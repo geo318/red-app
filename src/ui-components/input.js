35880a384ce515deb0c997d39630da72031c295a
geo318 <geo.lomidze@gmail.com>
2022-08-27 23:09:00 +0400

    some more validations

--- src/ui-components/input.js
+++ src/ui-components/input.js
@@ -2,16 +2,17 @@ import Error from "./error"
 export default function Input({ id, label, value, error, message, message_phone, handleChange, sub_type, ...inputProps }) {
 
     const handleError = () => {
-        error.pattern
+        return new RegExp(error?.pattern).test(value)
     }
+
+    
 
     return (
         <>
             <div className={`input${ inputProps.type ? ` input-${sub_type || inputProps.type}` : '' }`}>
                 { label && <label>{label}</label> }
-                <input id = {id} {...inputProps} value = {value} onChange = {handleChange} pattern = {inputProps.pattern} />
-                { (error && <Error error = {error}/>) || (message && <div>{message}</div>) }
-                {/* <span>{inputProps.errorMessage}</span> */}
+                <input id = {id} {...inputProps} value = {value} onChange = {handleChange} />
+                { (error && handleError() && <Error error = {error}/>) || (message && <div>{message}</div>) }
             </div>
         </>
     )
